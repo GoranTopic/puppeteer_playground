7656ead34bec7af99fd4a2eee7dd9b543385ef00
Goran Topic <telegonicaxx@live.com>
2022-04-14 12:35:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import puppeteer from 'puppeteer'
+
+async function get_proxies(){
+		const browser = await puppeteer.launch();
+		console.log('Opening browser...')
+		const page = await browser.newPage();
+		console.log('opened new page..')
+		await page.goto('https://free-proxy-list.net/');
+		const trs = await page.$$('tr')
+
+		for (const tr of trs) {
+				console.log( tr)
+		}
+
+		await browser.close();
+
+}
+
+async function main () {
+		const proxies = await get_proxies();
+		console.log('proxies', proxies);
+}
+
+main();
+
