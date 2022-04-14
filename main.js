2bd7331b770b81b8e71548d5e600551ed5ca289f
Goran Topic <telegonicaxx@live.com>
2022-04-14 13:53:31 -0700

    managed to get proxies

--- main.js
+++ main.js
@@ -6,14 +6,23 @@ async function get_them_proxies(){
 		const page = await browser.newPage();
 		console.log('opened new page..')
 		await page.goto('https://free-proxy-list.net/');
-		const trs = await page.$$('tr')
 
-		for (const tr of trs) {
-				console.log( tr)
-		}
+		// with xpath 
+		//let els = await page.$x('//tr/td[1]');
+		//for (let el of els)
+		//		console.log( await ( await el.getProperty('textContent') ).jsonValue() );
+
+		// waitin for selectos method and then for evey elemene, get the textContent?
+		let selector = 'textarea' // they are a area wher they have the 
+		//raw txt fo the proxies, got this by printing the text from the entire page
+		let element = await page.waitForSelector(selector)
+		let  txtEls = await element.evaluate( el => el.textContent )
+		let proxies = txtEls.split("\n")
+		//for (let proxy of proxies) console.log(proxy)
 
 		await browser.close();
 
+		return proxies;
 }
 
 async function main () {
