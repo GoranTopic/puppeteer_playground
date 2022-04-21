f1e061939e21025e433eaa3ca3bf13de5e7d0287
Goran Topic <telegonicaxx@live.com>
2022-04-20 22:30:36 -0700

    minor bug fix

--- supercias/scrap_names.js
+++ supercias/scrap_names.js
@@ -97,93 +97,93 @@ const get_last_recursion = () => {
 
 
 const scrap_dfst_company_names = async (page, depth) => {
-		/* this function scraps the name of the companie using a depth first search tree aproach */
-		let company_names = create_company_names(),
-				permutations = get_permutations(),
-				last_recursion = get_last_recursion(),
-				new_names = [],
-				max_rec = 4
-		// define recursive function
-		const recursive_search = async (str="", depth=0) => {
-				/* this is a recusrive function thath goes though all posible letter combination
-				 * based on the result that we find*/
-				// if we have alread seach those
-				//console.log('str:', str);
-				//console.log('last_recursion:', last_recursion);
-				// for every letter in the spanish alphabet
-				for( let i = 0; i < spanish_alphabet.length; i++ ){
-						let letter = spanish_alphabet.charAt(i);
-						//console.log(`(${str + letter} < ${last_recursion} ):`, (str + letter < last_recursion ));
-						if( str + letter < last_recursion ){
-								while(letter < last_recursion.charAt(depth)){
-										letter = spanish_alphabet.charAt(i++)
-										console.log(`getting new letter: ${letter}`)
-								}
-						}
-						// type letter
-						await text_input.type(letter, {delay: 100})
-						// wati for al network to over;
-						await waitUntilRequestDone(page, 500)
-						// see the result from new string
-						new_names = await get_company_names(page);
-						// print to view
-						await waitUntilRequestDone(page, 500)
-						console.log(`got this names with ${str+letter}`)
-						console.log(new_names)
-						// save names to the set
-						new_names.forEach(name => company_names.add(name));
-						// if there where few results
-						if( new_names.length <= 1 ){
-								// and the search string is very short
-								if( (str + letter).length === 1 ){ 
-										//console.log('str:', str)
-										//console.log('letter:', letter)
-										//console.log('str+letter:', str+letter)
-										// there must be an error, ABORT
-										console.error(`Error, got ${new_names.length} company names, with string ${str}`)
-										throw 'NameScrapingError'
-								}
-								// delete added charated
-								await page.keyboard.press('Backspace');
-								await waitUntilRequestDone(page, 500)
-								//console.log('few results found');
-								//console.log('EXITING recusiton');
-								return; // end recursion
-						}else { // good amount of names
-								// save names
-								write_json([ ...company_names], './resources/generated/company_names.json');
-								// continue recursion
-								//console.log("ENTERING recusrion");
-								//console.log('saving last recursion')
-								save_last_recursion(str + letter)
-								await recursive_search( str + letter, depth + 1 );
-								await page.keyboard.press('Backspace');
-								await waitUntilRequestDone(page, 500)
-						}
-				}
-		}
-		/* ---- start scraping proccess ---- */
-		// go to target url
-		await page.goto(target_url, { waitUntil: 'networkidle0', } );
-		// wait until the page is fully loaded
-		// get the radion name
-		const name_radio = (await page.$x("//label[text()='Nombre']/../input"))[0];
-		// click on the name radio
-		if (name_radio)  await name_radio.click();
-		// wait a second for page to load
-		await waitUntilRequestDone(page, 1000)
-		// get the main text input
-		const text_input = (await page.$x("//span[text()='Parámetro']/../input"))[0];
-		const real_text_input = (await page.$x("//span[text()='Parámetro']/../i/input"))[0];
-		const text_input_ID = await (await real_text_input.getProperty('id')).jsonValue();
-		// start recursive search
-		await recursive_search();
-		// take screen shot
-		await page.screenshot({
-				path: "./screenshot.png",
-				fullPage: true
-		});
-		return company_names;
+    /* this function scraps the name of the companie using a depth first search tree aproach */
+    let company_names = create_company_names(),
+        permutations = get_permutations(),
+        last_recursion = get_last_recursion(),
+        new_names = [],
+        max_rec = 4
+    // define recursive function
+    const recursive_search = async (str="", depth=0) => {
+        /* this is a recusrive function thath goes though all posible letter combination
+         * based on the result that we find*/
+        // if we have alread seach those
+        //console.log('str:', str);
+        //console.log('last_recursion:', last_recursion);
+        // for every letter in the spanish alphabet
+        for( let i = 0; i < spanish_alphabet.length; i++ ){
+            let letter = spanish_alphabet.charAt(i);
+            //console.log(`(${str + letter} < ${last_recursion} ):`, (str + letter < last_recursion ));
+            if( str + letter < last_recursion ){
+                while(letter < last_recursion.charAt(depth)){
+                    letter = spanish_alphabet.charAt(i++)
+                    console.log(`getting new letter: ${letter}`)
+                }
+            }
+            // type letter
+            await text_input.type(letter, {delay: 100})
+            // wati for al network to over;
+            await waitUntilRequestDone(page, 500)
+            // see the result from new string
+            new_names = await get_company_names(page);
+            // print to view
+            await waitUntilRequestDone(page, 500)
+            console.log(`got this names with ${str+letter}`)
+            console.log(new_names)
+            // save names to the set
+            new_names.forEach(name => company_names.add(name));
+            // if there where few results
+            if( new_names.length <= 1 ){
+                // and the search string is very short
+                if( (str + letter).length === 1 ){ 
+                    //console.log('str:', str)
+                    //console.log('letter:', letter)
+                    //console.log('str+letter:', str+letter)
+                    // there must be an error, ABORT
+                    console.error(`Error, got ${new_names.length} company names, with string ${str}`)
+                    throw 'NameScrapingError'
+                }
+                // delete added charated
+                await page.keyboard.press('Backspace');
+                await waitUntilRequestDone(page, 500)
+                //console.log('few results found');
+                //console.log('EXITING recusiton');
+                return; // end recursion
+            }else { // good amount of names
+                // save names
+                write_json([ ...company_names], 'mined_data/company_names.json');
+                // continue recursion
+                //console.log("ENTERING recusrion");
+                //console.log('saving last recursion')
+                save_last_recursion(str + letter)
+                await recursive_search( str + letter, depth + 1 );
+                await page.keyboard.press('Backspace');
+                await waitUntilRequestDone(page, 500)
+            }
+        }
+    }
+    /* ---- start scraping proccess ---- */
+    // go to target url
+    await page.goto(target_url, { waitUntil: 'networkidle0', } );
+    // wait until the page is fully loaded
+    // get the radion name
+    const name_radio = (await page.$x("//label[text()='Nombre']/../input"))[0];
+    // click on the name radio
+    if (name_radio)  await name_radio.click();
+    // wait a second for page to load
+    await waitUntilRequestDone(page, 1000)
+    // get the main text input
+    const text_input = (await page.$x("//span[text()='Parámetro']/../input"))[0];
+    const real_text_input = (await page.$x("//span[text()='Parámetro']/../i/input"))[0];
+    const text_input_ID = await (await real_text_input.getProperty('id')).jsonValue();
+    // start recursive search
+    await recursive_search();
+    // take screen shot
+    await page.screenshot({
+        path: "./screenshot.png",
+        fullPage: true
+    });
+    return company_names;
 }
 
 export { scrap_random_company_names, scrap_dfst_company_names }
