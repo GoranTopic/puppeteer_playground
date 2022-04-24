013a7f210eaee4cbedaa3829e55c065f2943cb64
Goran Topic <telegonicaxx@live.com>
2022-04-24 00:35:42 -0700

    working scrping ids

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
 import puppeteer from 'puppeteer';
 import { save_cookies, read_cookies, clean_company_names } from './utils.js'
 import spanish_alphbet from './resources/spanish_alphabet.js'
-import scrap_companies_ids from  './supercias/scrap_ids.js'
+import { scrap_ids, handle_home_page_click  } from  './supercias/scrap_ids.js'
 import browserOptions from './options/browser_options.js'
 
 async function main () {
 		// let's open a new browser
 		/* replace non breaking paces in scraped names */
-		//const sort = str => str.split('').sort((a, b) => a.localeCompare(b)).join('');
-		//clean_company_names('./mined_data/company_names.json')
-		//console.log(sort(spanish_alphbet))
-		//console.log( "00" > "0" )
-						
-		//console.log('Launching browser...')
+		let isScraping = true,
+				page;
+		// scraping 
 		const browser = await puppeteer.launch(browserOptions);
 		// get already opend page
-		const page = (await browser.pages())[0];
-		// read the cookies 
-		await read_cookies(page)
-		//const proxies = await get_them_proxies(browser);
-		//console.log('proxies', proxies);
-		//const names = await scrap_company_names(page);
-		//console.log('company names', names);
-		const ids = await scrap_companies_ids(page)
-		console.log('ids:', ids)
+		page = (await browser.pages())[0];
+		// start scraping
+		while(isScraping){
+				// read the cookies 
+				await read_cookies(page);
+				// handle the home page click
+				await handle_home_page_click(page);
+				// get new page opened
+				page = (await browser.pages())[0];
+				// scarp the ids of the with company names
+				isScraping = ! await scrap_ids(page);
+		}
 		// save the cookies
-		await save_cookies(page)
+		await save_cookies(page);
 		// let's close the browser
 		console.log('closing browser...')
 		await browser.close();
 }
-
 
 main();
--- supercias/scrap_ids.js
+++ supercias/scrap_ids.js
@@ -2,47 +2,113 @@ import { get_permutations } from '../permutations.js'
 import waitUntilRequestDone from '../waitForNetworkIdle.js'
 import { getText, read_json, write_json, save_cookies, read_cookies, } from '../utils.js'
 
-const scrap_companies = async page => {
-		// go to target url
-		let names = read_json('mined_data/company_names.json');
-		let target_url = 'https://appscvsmovil.supercias.gob.ec/PortalInfor/consultaPrincipal.zul'
-		await page.goto( target_url, // wait until the page is fully loaded
+
+const handle_home_page_click = async page => {
+		let home_url = 'https://www.supercias.gob.ec/portalscvs/'
+		// go to initial login page 
+		await page.goto( home_url, // wait until the page is fully loaded
 				{ waitUntil: 'networkidle0', }
 		);
-		//radio name button selector
-		const radio_selector = '//label[text()="Nombre"]/../input'
-		// get the radion 
-		const radio_el = (await page.$x(radio_selector))[0];
-		// click on the name radio
-		if (radio_el) await radio_el.click();
-		// until it loads the name
+		// get frame 
+		const iframeElement = await page.waitForSelector('iframe');
+		// get inside the frame
+		const frame = await iframeElement.contentFrame();
+		// selecte button
+		const button = await frame.waitForXPath('//span[text()="CONSULTA DE COMPAÑÍAS"]');
+		// click on button to go to company consulatas
+		button.click();
+		// select button
+		// wait until request is rendered
+		await waitUntilRequestDone(page, 500)
+		// wait for a while
 		await page.mainFrame().waitForTimeout(1000)
-		// get the main text input
-		const text_input = ( await page.$x("//span[text()='Parámetro']/../input") )[0];
-		const real_text_input = ( await page.$x("//span[text()='Parámetro']/../i/input") )[0];
-		const text_input_ID = await (await real_text_input.getProperty('id')).jsonValue();
-		// get button element  
-		const search_button = ( await page.$x("//td[text()='Buscar']/../../.."))[0];
-		//for( let name of names){
-		//type name of company
-		for( let letter of names[3] ){
-				await real_text_input.type(letter, {delay: 200})
-				await waitUntilRequestDone(page, 500)
-				console.log(letter);
-				if(letter === String.fromCharCode(160))
-						console.log('it is nonbreaking space')
-		}
-		//await text_input.type(names[0], {delay: 10})
-		// click search 
-		search_button.click()
-		// wait after button is pressed
-		
-		// take screen shot
-		await page.screenshot({
-				path: "./screenshot.png",
-				fullPage: true           
-		});
-		return null;
+		// close page after being done
+		await page.close()
 }
 
-export default scrap_companies
+
+// setters and getting for saving state
+const state_filename = 'resources/generated/id_scarp_state.js'
+const save_last_name_index = num => 
+		write_json({ "last_name" : num }, state_filename);
+const get_last_name_index = () => {
+		try{
+				return parseInt(read_json(state_filename)['last_name']);
+		}catch(e){
+				return "0"
+		}
+}
+
+const ids_output_file = 'mined_data/company_ids.json';
+const save_ids = ids => write_json(ids, ids_output_file);
+const get_ids = () => {
+		let res =  read_json(ids_output_file);
+		return res? res : [];
+}
+
+const scrap_ids = async page => {
+		/* scrap ids */
+		let names = read_json('mined_data/company_names.json'),
+				state_index = get_last_name_index(),
+				ids = get_ids(),
+				search_button,
+				text_input,
+				radio_el,
+				url,
+				id,
+				backButton;
+		// for every company names
+		try{ // catch all
+				for( let i = state_index; i < names.length; i++){ 
+						// get the radion 
+						radio_el = await page.waitForXPath('//label[text()="Nombre"]/../input');
+						// click on the name radio
+						if (radio_el) await radio_el.click();
+						// until it loads the name
+						await page.mainFrame().waitForTimeout(1000)
+						// wait
+						await waitUntilRequestDone(page, 1000)
+						// get the main text input
+						text_input = ( await page.$x("//span[text()='Parámetro']/../i/input") )[0];
+						// get button element  
+						search_button = ( await page.$x("//td[text()='Buscar']/../../.."))[0];
+						// type name of company
+						await text_input.type(names[i], {delay: 100});
+						// wait until for a little
+						await waitUntilRequestDone(page, 1000)
+						// click seach button
+						await search_button.click();
+						// wait until new page loads
+						await waitUntilRequestDone(page, 500);
+						// get the url value 
+						url = page.url();
+						// if  something whent wrong
+						if(url === "https://appscvsmovil.supercias.gob.ec/PortalInfor/consultaPrincipal.zul?id=1")
+								throw "Could not get url"
+						// get ids - TODO
+						id = url.split("=")[1];
+						console.log("page url", url);
+						console.log(`${i}/${names.length}`);
+						// push to list
+						ids.push({ company: names[i], id, url });
+						// save colected ids
+						save_ids(ids);
+						// save the name index
+						save_last_name_index(i);
+						// get back button 
+						backButton = ( await page.$x('//span[@class="z-button"]'))[0];
+						//console.log('backButton:', backButton);
+						// go back
+						await backButton.click();
+						// wait until page loads
+						await waitUntilRequestDone(page, 1000);
+				}
+		}catch(e){
+				console.error("something whent wrong!", e);
+				return false
+		}
+		conosle.log("done!")
+		return true
+}
+
+export { scrap_ids, handle_home_page_click }
